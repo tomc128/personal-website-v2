4c81114a0fa339a57b3a3ce246572cd9c1b84e05
Tom Chapman <tomchapman128@gmail.com>
2022-10-29 00:50:26 +0100

    Add nav docking - background appears when scrolled only

--- js/index.js
+++ js/index.js
@@ -1,9 +1,22 @@
+const dotGridContainer = document.querySelector('.dot-grid-container');
+const nav = document.querySelector('#nav');
+
 // Cool transform effect on SVG hero graphic
 document.addEventListener('mousemove', (e) => {
     const x = e.clientX / window.innerWidth;
     const y = e.clientY / window.innerHeight;
 
-    document.querySelectorAll('.dot-grid-container').forEach((item) => {
-        item.style.transform = `rotate(${x * 5}deg) skew(${x * 5}deg, ${y * 5}deg)`;
-    });
+    dotGridContainer.style.transform = `rotate(${x * 5}deg) skew(${x * 5}deg, ${y * 5}deg)`;
+});
+
+// on scroll
+window.addEventListener('scroll', () => {
+    const scrollPosition = window.scrollY;
+
+    if (scrollPosition > 10 && nav.classList.contains('docked')) {
+        nav.classList.remove('docked');
+    }
+    if (scrollPosition <= 10 && !nav.classList.contains('docked')) {
+        nav.classList.add('docked');
+    }
 });
