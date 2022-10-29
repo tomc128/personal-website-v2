6e2a12bdfbfc732beb8d5906a217b23322c02221
Tom Chapman <tomchapman128@gmail.com>
2022-10-29 14:25:38 +0100

    Add custom on load animations with data attributes

--- js/index.js
+++ js/index.js
@@ -81,3 +81,17 @@ projectCards.forEach((projectCard) => {
         currentProjectCardImage.style.transform = '';
     });
 });
+
+
+const onLoad = document.querySelectorAll('[data-load]');
+
+window.addEventListener('load', (event) => {
+    onLoad.forEach((element) => {
+        const delay = element.dataset.loadDelay;
+
+        setTimeout(() => {
+            element.classList.add('loaded');
+        }, delay);
+
+    });
+});
